ccbdea5e0b7fdcaeff8b59257eef1436eec1535e
leo <leozhang621@gmail.com>
2018-01-25 11:06:17 +0800

    finished tapBall

--- tapBall/index.js
+++ tapBall/index.js
@@ -17,12 +17,23 @@ function setCanvasSize () {
     canvas.height = window.innerHeight
 }
 
-function creatBall (x,y) {
+function setBallEndPos(p) {
+    let angle = anime.random(0,360)*Math.PI/180
+    let value = anime.random(50,180)
+    let radius = [-1,1][anime.random(0,1)] * value
+    return {
+        x: p.x + radius*Math.cos(angle),
+        y: p.y + radius*Math.sin(angle)
+    }
+}
+
+function createBall (x,y) {
     let p = {}
     p.x = x
     p.y = y
     p.color = colors[anime.random(0,colors.length - 1)]
     p.radius = anime.random(16,32)
+    p.endPos = setBallEndPos(p)
     p.draw = function(){
         ctx.beginPath()
         ctx.arc(p.x,p.y,p.radius,0,Math.PI*2,true)
@@ -32,15 +43,16 @@ function creatBall (x,y) {
     return p
 }
 
-function creatCircle (x,y) {
+function createCircle (x,y) {
     let p = {}
     p.x = x
     p.y = y
     p.radius = 0.1
     p.lineWidth = 6
     p.color = '#fff'
+    p.alpha = 0.5
     p.draw = () => {
-        ctx.globalAlpha = 0.5
+        ctx.globalAlpha = p.alpha
         ctx.beginPath()
         ctx.arc(p.x,p.y,p.radius,0,2*Math.PI,true)
         ctx.lineWidth = p.lineWidth
@@ -56,27 +68,59 @@ function animation(x,y) {
     let circle = createCircle(x,y)
     let balls = []
     for(let i = 0; i< ballNumbers; i++) {
-        balls.push(creatBall(x,y))
+        balls.push(createBall(x,y))
     }
-    
+    anime.timeline()
+    .add({
+        targets: balls,
+        x: p => p.endPos.x,
+        y: p => p.endPos.y,
+        radius: 0.1,
+        duration: anime.random(1200, 1800),
+        easing: 'easeOutExpo',
+        update: render,
+    })
+    .add({
+        targets: circle,
+        radius: anime.random(80,150),
+        lineWidth: 0,
+        round:1,
+        alpha: {
+            value: 0,
+            easing: 'linear',
+            duration: anime.random(600,800)
+        },
+        duration: anime.random(1200, 1800),
+        easing: 'easeOutExpo',
+        update: render,
+        offset: 0
+    })    
 }
+
+function render(animeManager) {
+    Array.prototype.forEach.call(animeManager.animatables,(item,index)=>{
+        item.target.draw()
+    })
+}
+
 
 canvas.addEventListener('click',(event)=>{
     pointer.x = event.clientX
     pointer.y = event.clientY
     clear.play()
-    let circle = creatCircle(pointer.x,pointer.y)
-    anime({
-        targets: circle,
-        radius: anime.random(80,150),
-        lineWidth: 0,
-        round:1,
-        duration: 300,
-        easing: 'easeOutExpo',
-        update: function(){
-            circle.draw()
-        }
-    })
+    animation(pointer.x,pointer.y)
+    // let circle = creatCircle(pointer.x,pointer.y)
+    // anime({
+    //     targets: circle,
+    //     radius: anime.random(80,150),
+    //     lineWidth: 0,
+    //     round:1,
+    //     duration: 300,
+    //     easing: 'easeOutExpo',
+    //     update: function(){
+    //         circle.draw()
+    //     }
+    // })
 })
 
 window.addEventListener('resize',(event)=>{
