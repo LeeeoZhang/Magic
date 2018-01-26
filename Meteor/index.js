22dc886bbdda4d3957b56369d848d5c5c2441753
leo <leozhang621@gmail.com>
2018-01-26 16:13:35 +0800

    finished Meteor

--- Meteor/index.js
+++ Meteor/index.js
@@ -2,20 +2,25 @@ const canvas = document.getElementById('meteor')
 const ctx = canvas.getContext('2d')
 const startNum = 30
 const canvasSize = {
-    width: canvas.width,
-    height: canvas.height,
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight,
 }
-const speedList = [10,20,30,40,50,60]
-const lengthList = [50,60,70,80,90,100]
+const speedList = [5, 6, 7, 8, 9, 10]
+const lengthList = [10, 15, 20, 25, 30, 35]
 const speedRatio = 0.8
 
 let resizeTimer
-let 
+let requestAnimationId
 let starts = []
 
 //计算随机数字
 function random (num1, num2) {
     return Math.floor(Math.random() * Math.abs(num2 - num1) + Math.min(num1, num2))
+}
+
+//清除画布
+function clear () {
+    ctx.clearRect(0, 0, canvasSize.width, canvasSize.height)
 }
 
 //计算画布大小
@@ -27,8 +32,8 @@ function setCanvasSize () {
 //随机生成开始坐标
 function randomInitCoordinate () {
     let x, y
-    x = Math.floor(Math.random() * (canvasSize.width - 600) + 300)
-    y = 0
+    x = Math.floor(Math.random() * canvasSize.width)
+    y = Math.floor(Math.random() * 200)
     return {
         x, y
     }
@@ -37,10 +42,18 @@ function randomInitCoordinate () {
 //随机生成结束坐标
 function randomEndCoordinate () {
     let x, y
-    x = Math.floor(Math.random() * (canvasSize.width - 600) + 300)
-    y = Math.floor(Math.random() * canvasSize.height / 2 + 300)
+    x = Math.floor(Math.random() * canvasSize.width)
+    y = Math.floor(Math.random() * canvasSize.height + 300)
     return {
         x, y
+    }
+}
+
+//检查流星坐标变化
+function checkCoordinate (start) {
+    if (start.sx > start.ex && start.sy > start.ey) {
+        start.sx = start.ix
+        start.sy = start.iy
     }
 }
 
@@ -54,9 +67,9 @@ class Meteor {
         this.ex = randomEndCoordinate().x
         this.ey = randomEndCoordinate().y
         //流星速度
-        this.speed = speedList[random(0,5)]
+        this.speed = speedList[random(0, 5)]
         //流星长度
-        this.length = lengthList[random(0,5)]
+        this.length = lengthList[random(0, 5)]
         //保存流星初始坐标
         this.ix = this.sx
         this.iy = this.sy
@@ -64,26 +77,31 @@ class Meteor {
     }
 
     updateCoordinate () {
-        this.ix += this.speed * speedRatio
-        this.iy += this.speed
+        this.sx += this.speed * speedRatio
+        this.sy += this.speed
     }
 }
 
 //生成流星
 function createStarts () {
-    starts.push(new Meteor())
+    for (let i = 0; i < startNum; i++) {
+        starts.push(new Meteor())
+    }
 }
 
 //渲染流星
 function render () {
-    for(let i =0 ; i<starts.length;i++) {
+    clear()
+    for (let i = 0; i < starts.length; i++) {
         ctx.strokeStyle = '#fff'
         ctx.beginPath()
-        ctx.moveTo(starts[i].sx,starts[i].sy)
-        ctx.lineTo(starts[i].sx - starts[i].length*Math.cos(Math.PI/3),starts[i].sx - starts[i].length*Math.sin(Math.PI/3))
+        ctx.moveTo(starts[i].sx, starts[i].sy)
+        ctx.lineTo(starts[i].sx - starts[i].length * Math.cos(Math.PI / 4), starts[i].sy - starts[i].length * Math.sin(Math.PI / 4))
         ctx.stroke()
+        starts[i].updateCoordinate()
+        checkCoordinate(starts[i])
     }
-
+    requestAnimationId = requestAnimationFrame(render)
 }
 
 
@@ -97,3 +115,5 @@ window.addEventListener('resize', () => {
 
 
 setCanvasSize()
+createStarts()
+render()
