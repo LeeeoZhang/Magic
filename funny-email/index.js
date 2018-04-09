b39e1436910d9dd3f1394a2dc2bda8a2ae7ba94b
leo <leozhang621@gmail.com>
2018-04-09 10:37:06 +0800

    modified

--- funny-email/index.js
+++ funny-email/index.js
@@ -22,14 +22,30 @@ function backgroundColorAnime(){
     })
 }
 
+function toSuccess (){
+    anime({
+        targets:'#messageOutline',
+        d:{
+            value:[
+                'M242.1,265.7l52.4,52.3l51.1-52.3',
+                'M308,312.3l-26.2-26 M281.8,313.3l26.2-27'
+            ]
+        },
+        easing:'easeOutSine',
+        duration: 500,
+    })
+}
+
+
+
 function popAnime(){
     anime({
         targets:'#container #popGroup line',
-        strokeDashoffset: [0, anime.setDashoffset],
-        easing:'easeInOutSine',
-        duration: 1500,
-        direction:'alternate'
+        strokeDashoffset: [0,anime.setDashoffset],
+        easing:'easeOutSine',
+        duration: 300,
     })
 }
-popAnime()
+toSuccess()
+//popAnime()
 // backgroundColorAnime()
